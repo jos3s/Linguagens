73d27e3e31ce5e8c12130818f6cfd240f908bb4b
jos3s <karatedkgt17@gmail.com>
2019-12-13 22:33:14 -0300

    Adição/Recriação do código de MIPS em Pyhton

---
+++ pagamento_python.py
@@ -0,0 +1,22 @@
+bit=int(input('Digite o bit de verificação:'))
+cn='Compra negada'
+cs='Compra concluida com sucesso'
+
+if bit==0 :
+    valor=int(input('Digite o valor da compra: '))
+    if valor<=1000 :
+        cartao=input('Digite o numero do cartão: ')
+        tam=len(cartao)
+        print(tam)
+        if tam==16:
+            senha=int(input('Digite a senha do cartão: '))
+            print('\n')
+            print('O valor da compra foi {}'.format(valor))
+            print('O número do cartão é {}'.format(cartao))
+            print(cs)
+        else :
+            print(cn)
+    else :
+        print(cn)
+else :
+    print(cn)
