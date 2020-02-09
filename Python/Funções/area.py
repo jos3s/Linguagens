dd0b9edc9df458d6eba6ae5d8e19056347e0b22a
jos3s <karatedkgt17@gmail.com>
2020-02-09 12:50:45 -0300

    Adição/Código com funções

---
+++ Python/Funções/area.py
@@ -0,0 +1,8 @@
+def area( l,c):
+    return l*c
+
+
+larg=float(input('Digite a largura: '))
+comp=float(input('Digite o comprimento: '))
+a=area(larg,comp)
+print(f'A área é {a}, a largura passada foi {larg} e o comprimento foi {comp}')
---
+++ Python/Funções/printFormatado.py
@@ -0,0 +1,9 @@
+def printf(msg):
+    tam=len(msg)+2
+    print('~'*tam)
+    print(f' {msg} ')
+    print('~'*tam)
+    
+    
+printf('Hello Word')
+printf('Olá Mundo')
